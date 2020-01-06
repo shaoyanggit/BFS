dfefeb4cb4065619df78dd53ce6174b4eca38c30
Sam Yang <samabc75@gmail.com>
2020-01-06 09:25:22 +0800

    fix bug

--- server/full_text_crawler.py
+++ server/full_text_crawler.py
@@ -3,8 +3,10 @@ import csv
 import requests
 import re
 from bs4 import BeautifulSoup
+import datetime
 
 def craw_data(out_file, target_file=None, target=None):
+    start_time = datetime.datetime.now()
     targets = []
     if target:
         targets.append(target)
@@ -17,7 +19,7 @@ def craw_data(out_file, target_file=None, target=None):
     pdf = 0
 
     for i, target in enumerate(targets):
-        print(i)
+        print(i, target['title'])
         
         # skip pdf, cannot hadle pdf now
         if re.search(".pdf$", target['url']):
@@ -25,7 +27,7 @@ def craw_data(out_file, target_file=None, target=None):
             continue
 
         try:
-            r = requests.get(target['url'])
+            r = requests.get(target['url'], timeout=10)
             if r.status_code == requests.codes.ok:
                 soup = BeautifulSoup(r.text, 'lxml')
                 if soup.body != None:
@@ -34,9 +36,12 @@ def craw_data(out_file, target_file=None, target=None):
                     plain_text = soup.body.get_text()
                     plain_text = re.sub(r' {2,}', " ", plain_text)
                     plain_text = re.sub(r'[\t\n]', "", plain_text)
+                    if len(plain_text) > 131072:
+                        plain_text = plain_text[:131072]
             
             data.append({'title': target['title'], 'url': target['url'], 'plain_text': plain_text})
         except:
+            print('error')
             error += 1
         
         # if i >= 100:
@@ -54,4 +59,6 @@ def craw_data(out_file, target_file=None, target=None):
             writer = csv.DictWriter(outfile, fieldnames=fieldnames)
             writer.writerows(data)
 
+    end_time = datetime.datetime.now()
+    print('start at {}, end at {}'.format(start_time, end_time))
     print("done with {} errors in {} data. skip with {} pdf".format(error, len(targets), pdf))
